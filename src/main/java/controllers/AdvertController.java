3bf194583fa0f5dcd0176954c873154c408f0e48
Stephen Rooney <s.f.rooney@gmail.com>
2018-07-19 16:40:01 +0100

    landing page has landed

--- src/main/java/controllers/AdvertController.java
+++ src/main/java/controllers/AdvertController.java
@@ -1,8 +1,44 @@
 package controllers;
 
+import db.DBHelper;
 import db.Seeds;
+import models.Advert;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.get;
 
 public class AdvertController {
 
+    public class EmployeesController {
+        public EmployeesController(){
+            this.setupEndPoints();
 
+
+        }
+
+        public void setupEndPoints(){
+
+
+
+
+
+            get("/adverts", (req,res) ->{
+                Map<String, Object> model = new HashMap();
+
+
+                List<Advert> adverts = DBHelper.getAll(Advert.class);
+                model.put("adverts", adverts);
+                model.put("template", "templates/adverts/index.vtl");
+
+                return new ModelAndView(model, "templates/layout.vtl");
+
+            }, new VelocityTemplateEngine());
+
+        }
+    }
 }
