c82a27dceeffecc4281e2efe9c87ba3cca1ce360
Stephen Rooney <s.f.rooney@gmail.com>
2018-07-19 16:14:03 +0100

    seeds made

---
+++ src/main/java/Runner.java
@@ -0,0 +1,29 @@
+import db.DBHelper;
+import models.*;
+
+import java.util.ArrayList;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+
+        User user = new User("Dave");
+        DBHelper.save(user);
+        Item item = new Item("Amazing Photography", "Y", user, Category.CLOTHES, "1234");
+        DBHelper.save(item);
+        Artist artist = new Artist("Amazing paint master", "If you need a cool design in your weird coffee shop, this is your guy", user, "Mike", Skill.PAINTER, "Edinburgh");
+        DBHelper.save(artist);
+
+        ArrayList adverts = new ArrayList<Advert>();
+
+        adverts.add(artist);
+        adverts.add(item);
+        user.setAdverts(adverts);
+        DBHelper.save(user);
+
+
+
+
+    }
+    }
---
+++ src/main/java/db/Seeds.java
@@ -0,0 +1,28 @@
+package db;
+
+import models.*;
+
+import java.util.ArrayList;
+
+public class Seeds {
+    public static  void seedData(){
+
+        DBHelper.deleteAll(Advert.class);
+        DBHelper.deleteAll(Artist.class);
+        DBHelper.deleteAll(Item.class);
+        DBHelper.deleteAll(User.class);
+        User user = new User("Dave");
+        DBHelper.save(user);
+        Item item = new Item("Amazing Photography", "Y", user, Category.CLOTHES, "1234");
+        DBHelper.save(item);
+        Artist artist = new Artist("Amazing paint master", "If you need a cool design in your weird coffee shop, this is your guy", user, "Mike", Skill.PAINTER, "Edinburgh");
+        DBHelper.save(artist);
+
+        ArrayList adverts = new ArrayList<Advert>();
+
+        adverts.add(artist);
+        adverts.add(item);
+        user.setAdverts(adverts);
+        DBHelper.save(user);
+    }
+}
