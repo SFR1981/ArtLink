e5b9d0249ad819dc1d939680d3d4ed0c768850c1
Stephen Rooney <s.f.rooney@gmail.com>
2018-07-19 13:38:25 +0100

    tests for User

---
+++ src/main/java/models/User.java
@@ -0,0 +1,61 @@
+package models;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name= "users")
+public class User {
+
+    private int id;
+    private String username;
+    private List<Advert> adverts;
+    private String image;
+
+    public User(){
+
+    }
+
+    public User(String username){
+        this.username = username;
+        this.image = null;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "username")
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    public List<Advert> getAdverts() {
+        return adverts;
+    }
+
+    public void setAdverts(List<Advert> adverts) {
+        this.adverts = adverts;
+    }
+
+    @Column(name ="images")
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
---
+++ src/test/java/models/UserTest.java
@@ -0,0 +1,37 @@
+package models;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+
+    @Before
+    public void before(){
+    }
+
+    @Test
+    public void getUsername() {
+    }
+
+    @Test
+    public void setUsername() {
+    }
+
+    @Test
+    public void getAdverts() {
+    }
+
+    @Test
+    public void setAdverts() {
+    }
+
+    @Test
+    public void getImage() {
+    }
+
+    @Test
+    public void setImage() {
+    }
+}
