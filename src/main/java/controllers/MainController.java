e4f4981728b503c4169e6bcc737c30993a318686
Stephen Rooney <s.f.rooney@gmail.com>
2018-07-20 15:46:24 +0100

    public

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -8,10 +8,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.SparkBase.staticFileLocation;
 
 public class MainController {
 
     public static void main(String[] args) {
+
+        staticFileLocation("/public");
 
         Seeds.seedData();
 
