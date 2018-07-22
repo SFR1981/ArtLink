e175cd1d37239e6366a4fc77310d89201fe76ac7
Stephen Rooney <s.f.rooney@gmail.com>
2018-07-22 17:59:55 +0100

    making user based changes

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -1,10 +1,13 @@
 package controllers;
 
+import db.DBHelper;
 import db.Seeds;
+import models.User;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.get;
@@ -28,7 +31,19 @@ public class MainController {
 
         get("/", (req,res)-> {
             Map<String,Object> model = new HashMap<>();
+            List<User> users = DBHelper.getAll(User.class);
+            model.put("users", users);
             model.put("template", "templates/landing.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+
+        get("/:id", (req,res)-> {
+            Map<String,Object> model = new HashMap<>();
+            int id = Integer.parseInt(req.params(":id"));
+            User user = DBHelper.find(id, User.class);
+            model.put("user", user);
+            model.put("template", "templates/main.vtl");
             return new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
 
